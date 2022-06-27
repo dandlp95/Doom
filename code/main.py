77f61a19279664f0183cda80d101483f5f682a90
dandlp95 <del17011@byui.edu>
2022-06-27 03:34:07 -0600

    hcanges to animation and design

--- code/main.py
+++ code/main.py
@@ -8,7 +8,7 @@ from enemy import Enemy
 
 pygame.init() # Start pygame
 
-screen = pygame.display.set_mode((1400,600)) # Creates Screen
+screen = pygame.display.set_mode((682,512)) # Creates Screen
 
 mixer.music.load("../media/Doom.wav")
 mixer.music.play(-1)
@@ -17,7 +17,7 @@ mixer.music.play(-1)
 pygame.display.set_caption("Doom")
 icon = pygame.image.load("../media/001-pentagram.png")
 pygame.display.set_icon(icon)
-background = pygame.image.load("../media/background2.jpg")
+background = pygame.image.load("../media/hell.jpg")
 
 # FPS
 clock = pygame.time.Clock()
@@ -28,11 +28,14 @@ running = True
 # Character
 player_walk_count = 0
 
+# Faces
+faces = ["../media/player/faces/doom-face1.png", "../media/player/faces/doom-face2.png", "../media/player/faces/doom-face1.png", "../media/player/faces/doom-face3.png", "../media/player/faces/doom-face1.png", "../media/player/faces/doom-face2.png", "../media/player/faces/doom-face1.png", "../media/player/faces/doom-face3.png", "../media/player/faces/doom-face1.png"]
+
 # Setting up character
 character = Player()
 
-character.set_x(200)
-character.set_y(400)
+character.set_x(600)
+character.set_y(300)
 character.set_velocity(5)
 character.set_height(64)
 character.set_width(64)
@@ -43,13 +46,20 @@ left = False
 x = character.get_x()
 y = character.get_y()
 
+frame = 0
+
 def redraw_screen():
     
     # background.
     screen.blit(background, (0,0))
     
     global player_walk_count
+    global frame
     
+    # if frame + 1 >= 27:
+    #     frame = 0
+        
+    # screen.blit(pygame.image.load(faces[frame//3]), (20, 20))
     
     if player_walk_count + 1 >= 27:
         player_walk_count = 0
@@ -58,19 +68,24 @@ def redraw_screen():
         walk_left_animation = character.get_walkLeft_animation()
         screen.blit(pygame.image.load(walk_left_animation[player_walk_count//3]), (x, y))
         player_walk_count += 1
+        screen.blit(pygame.image.load(faces[3]), (20, 20))
         
     elif character.get_right():
         
         walk_right_animation = character.get_walkRight_animation()
         screen.blit(pygame.image.load(walk_right_animation[player_walk_count//3]), (x,y))
         player_walk_count +=1
+        screen.blit(pygame.image.load(faces[1]), (20, 20))
     else:
         screen.blit(pygame.image.load(character.get_image()), (x,y))
+        screen.blit(pygame.image.load(faces[0]), (20, 20))
     
     pygame.display.update()
 
 while running:
     clock.tick(27)
+    frame += 1
+    
     
     # Anything persistant goes inside this loop
     for event in pygame.event.get():
--- code/player.py
+++ code/player.py
@@ -5,7 +5,8 @@ class Player(Actor):
     def __init__(self):
         super().__init__()
         self._lives = 0
-        self._image = '../media/player/standing.png'
+        #self._image = '../media/player/standing.png'
+        self._image = "../media/player/doom-guy.png"
         self._walk_right = ['../media/player/R1.png', '../media/player/R2.png', '../media/player/R3.png', '../media/player/R4.png', '../media/player/R5.png', '../media/player/R6.png', '../media/player/R7.png', '../media/player/R8.png', '../media/player/R9.png']
         self._walk_left = ['../media/player/L1.png', '../media/player/L2.png', '../media/player/L3.png', '../media/player/L4.png', '../media/player/L5.png', '../media/player/L6.png', '../media/player/L7.png', '../media/player/L8.png', '../media/player/L9.png']
         
